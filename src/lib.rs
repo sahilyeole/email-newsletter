aaa9dba8e29669b127ba3234340c691c4fc7be96
Sahil Yeole <sahilyeole93@gmail.com>
2024-03-19 00:46:58 +0530

    add: subscription serialization

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,27 @@ use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
 
+#[derive(serde::Deserialize, std::fmt::Debug)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
